f80f5bf1768c215ca16035da6e1a1c5463856b20
tw2013.kim <tigger6392@gmail.com>
2023-06-02 08:42:35 +0900

    First commit

---
+++ Project1/baseball.cpp
@@ -0,0 +1,5 @@
+class Baseball
+{
+public:
+
+};
---
+++ Sample-Test1/baseball_test.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "../Project1/baseball.cpp"
+
+TEST(BaseballGame, TryGameTest) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
