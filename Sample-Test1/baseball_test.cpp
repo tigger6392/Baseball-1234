2aa7e05bfcfdf48321d413ed8c8be2049656073e
tw2013.kim <tigger6392@gmail.com>
2023-06-02 10:10:13 +0900

    Implementations and tests added
    Signed-off-by: tw2013.kim <tigger6392@gmail.com>

--- Sample-Test1/baseball_test.cpp
+++ Sample-Test1/baseball_test.cpp
@@ -1,7 +1,28 @@
 #include "pch.h"
 #include "../Project1/baseball.cpp"
 
-TEST(BaseballGame, TryGameTest) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(BaseballGame, InvalidArguemnt) {
+	Baseball baseball("123");
+	EXPECT_THROW(baseball.guess(""), std::invalid_argument);
+	EXPECT_THROW(baseball.guess("1543"), std::invalid_argument);
+	EXPECT_THROW(baseball.guess("1v3"), std::invalid_argument);
+	EXPECT_THROW(baseball.guess("121"), std::invalid_argument);
+}
+
+TEST(BaseballGame, PerfectMatch) {
+	Baseball baseball("123");
+	Result result = baseball.guess("123");
+
+	EXPECT_EQ(true, result.solved);
+	EXPECT_EQ(3, result.strikes);
+	EXPECT_EQ(0, result.balls);
+}
+
+TEST(BaseballGame, MissMatch) {
+	Baseball baseball("123");
+	Result result = baseball.guess("789");
+
+	EXPECT_EQ(false, result.solved);
+	EXPECT_EQ(0, result.strikes);
+	EXPECT_EQ(0, result.balls);
 }
